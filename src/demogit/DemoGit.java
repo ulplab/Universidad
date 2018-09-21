8640b9628b46ed3c6ca65516a4543547a1f83d12
ulplab <agagliardi@ulp.edu.ar>
2018-09-20 23:01:21 -0300

    commnet

--- src/demogit/DemoGit.java
+++ src/demogit/DemoGit.java
@@ -17,7 +17,7 @@ public class DemoGit {
     public static void main(String[] args) {
         //generemos conflicto
         System.out.println("Hola Git!");
-        //Agrego uncomentario
+        //Agrego uncomentario malo
         //que pasa aca
     }
     
